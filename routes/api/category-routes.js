c1d3ce1b13e17adfa21c2625e541513b9cac856e
maaront <35611834+maaront@users.noreply.github.com>
2023-05-20 19:36:54 -0400

    create delete categroy by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -64,6 +64,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-});
+  Category.destroy({ where: { id: req.params.id }
+    .then((categoryData) => {
+      res.status(200).json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    }),
+  });
+  });
 
 module.exports = router;
