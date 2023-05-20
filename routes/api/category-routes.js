84a96d32ea272c12cff3c5d91a65f3f190bb6c80
maaront <35611834+maaront@users.noreply.github.com>
2023-05-20 19:27:22 -0400

    create post route for creating new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,10 +36,28 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-});
+  Category.create({ category_name: req.body.category_name })
+    .then((categoryData) => {
+      res.status(200).json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+  
+  });
+  });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.create({ category_name: req.body.category_name })
+  .then((categoryData) => {
+    res.status(200).json(categoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+
+});
 });
 
 router.delete('/:id', (req, res) => {
