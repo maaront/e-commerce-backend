51ad608b7a7e7406e87b74d5beae91aa96707303
maaront <35611834+maaront@users.noreply.github.com>
2023-05-20 19:29:15 -0400

    create update categry by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,16 +49,18 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.create({ category_name: req.body.category_name })
+  Category.update({ category_name: req.body.category_name }, { where: { id: req.params.id }
   .then((categoryData) => {
     res.status(200).json(categoryData);
   })
   .catch((err) => {
     console.log(err);
     res.status(500).json(err);
+  }),
+});
+});
 
-});
-});
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
